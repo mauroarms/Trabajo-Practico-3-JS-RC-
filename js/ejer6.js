4f2ffdd098492af0a3818c9c507626ecc5a5dbdb
mauroarms <mauro.armas14@gmail.com>
2023-11-06 22:37:22 -0300

    Trabajo Practico 3 finalizado

--- js/ejer6.js
+++ js/ejer6.js
@@ -1,17 +1,3 @@
-/*
-6- Solicitar por pantalla al usuario ingresar el valor de los lados de un rectángulo, luego crear una función para calcular su perímetro y mostrarlo por pantalla.
-
-La fórmula del perímetro  es p = 2*(a +b)
-
-Ejemplo:
-
-Input:
-lado A = 24
-lado B = 5
-
-Output: 58
-*/
-
 function rectangulo(altura, base){
     const p = 2*(altura+base)
     return p
--- js/ejer7.js
+++ js/ejer7.js
@@ -1,9 +1,58 @@
-/*
+function producto(a){
+    const tabla = `
+    <table>
+        <tbody>
+            <thead>
+                <td>Multiplicador</td>
+                <td>Resultado</td>
+            </thead> 
+            <tr>       
+            <tr>       
+                <td>1</td>
+                <td> ${a*1} </td>
+            </tr>
+            <tr>       
+                <td>2</td>
+                <td> ${a*2} </td>
+            </tr>
+            <tr>
+                <td>3</td>
+                <td> ${a*3} </td>
+            </tr>
+            <tr>       
+                <td>4</td>
+                <td> ${a*4} </td>
+            </tr>
+            <tr>
+                <td>5</td>
+                <td> ${a*5} </td>
+            </tr>
+            <tr>       
+                <td>6</td>
+                <td> ${a*6} </td>
+            </tr>
+            <tr>
+                <td>7</td>
+                <td> ${a*7} </td>
+            </tr>
+            <tr>       
+                <td>8</td>
+                <td> ${a*8} </td>
+            </tr>
+            <tr>       
+                <td>9</td>
+                <td> ${a*9} </td>
+            </tr>
+            <tr>       
+                <td>10</td>
+                <td> ${a*10} </td>
+            </tr>
+        </tbody>
+    </table>
+    `     
+    return tabla
+}
 
-
-Dificultad:  🟢🟡
-7- Escriba un script que muestre la tabla de multiplicar de un número ingresado por pantalla, la creación de la tabla debe ser realizada con una función y mostrar solo los resultados del 1 al 10 del número elegido por el usuario.
-
-
-*/
-
+const num = parseInt(prompt("Ingrese el numero a multiplicar: "))
+document.write(producto(num))
+console.log(producto(num))
