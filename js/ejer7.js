ff1a0adfe6945c28a7c04dbc1226e8afe9fa97ab
mauroarms <mauro.armas14@gmail.com>
2023-11-06 21:28:54 -0300

    Se ralizo el ejercicio 4 correctamente

---
+++ js/ejer7.js
@@ -0,0 +1,26 @@
+/*
+4- Escribir el código de una función a la que se pasa como parámetro un número entero y devuelve como resultado una cadena de texto que indica si el número es par o impar. Mostrar por pantalla el resultado devuelto por la función.
+
+Dificultad:  🟢🟡
+5- Definir una función que muestre información sobre una cadena de texto que se le pasa como argumento. A partir de la cadena que se le pasa, la función determina si esa cadena está formada sólo por mayúsculas, sólo por minúsculas o por una mezcla de ambas.
+Dificultad:  🟢🟡
+6- Solicitar por pantalla al usuario ingresar el valor de los lados de un rectángulo, luego crear una función para calcular su perímetro y mostrarlo por pantalla.
+
+La fórmula del perímetro  es p = 2*(a +b)
+
+Ejemplo:
+
+
+
+
+Input:
+lado A = 24
+lado B = 5
+
+Output: 58
+
+Dificultad:  🟢🟡
+7- Escriba un script que muestre la tabla de multiplicar de un número ingresado por pantalla, la creación de la tabla debe ser realizada con una función y mostrar solo los resultados del 1 al 10 del número elegido por el usuario.
+
+
+*/
